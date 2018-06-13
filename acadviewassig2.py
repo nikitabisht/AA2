e93e20efb10abd3d4493cd368047d3e7fdf94743
nikitabisht <nikitabisht300@gmail.com>
2018-06-13 22:01:09 +0530

    assig 2 done

---
+++ acadviewassig2.py
@@ -0,0 +1,34 @@
+#question1 print anything you want on screen.
+
+print("HELLO WORLD")
+
+#question2 join two string using concatenation.
+
+str1="acad"
+str2="view"
+print(str1+str2)
+
+#question3 take the input of three variables x,y and z . print there value on screen
+
+x=1
+y=2
+z=3
+print(x, y, z)
+
+#question4 print let's get started on screen.
+
+print("let\'s get started on screen")
+
+#question5
+
+s="Acadview"
+course="python"
+fees=5000
+print("welcome to %s, course is %s and fees is %d" % (s,course,fees))
+
+#question6
+
+name = "tony stark"
+salary = 1000000
+print('%s%d'%(name,salary))
+
